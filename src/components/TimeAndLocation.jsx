a2317e991c88315c1f04a80ed4f8a81cbd472fcb
Cuong Mai <cuongmai@Cuongs-MacBook-Pro.local>
2024-01-15 17:46:59 -0500

    first commit

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className="flex itmes-center justify-center my-6">
+            <p className="text-white text-xl font-extralight">
+                Tuesday, 25 September 2023 | Local time: 12:45 PM
+            </p>
+
+        </div>
+
+        <div className="flex itmes-center justify-center my-3">
+            <p className="text-white text-3xl font-medium">
+                Berlin, DE
+            </p>
+
+        </div>
+
+
+    
+    </div>
+    
+  );
+}
+
+export default TimeAndLocation
